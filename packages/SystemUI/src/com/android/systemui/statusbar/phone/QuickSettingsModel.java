e0701b9fd88d79812c7365f337c218be9c04d508
Winson Chung <winsonc@google.com>
2013-10-17 18:26:00 -0700

    DO NOT MERGE Fix null pointer exception when updating rotation tile. (Bug 11111473)
    This occurs when switching users on devices that
    don't have a rotation lock quick settings tile.
    
    Change-Id: I738ca15ca754be90cec7eaf3a8eda3e38ed96b33
    (cherry picked from commit e8f50a1400d288e6fd7c20ec7947270e204ee57d)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
@@ -290,7 +290,7 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
         mUserTracker = new CurrentUserTracker(mContext) {
             public void onUserSwitched(int newUserId) {
                 mBrightnessObserver.startObserving();
-                onRotationLockChanged();
+                refreshRotationLockTile();
                 onBrightnessLevelChanged();
                 onNextAlarmChanged();
                 onBugreportChanged();
