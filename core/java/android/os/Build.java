39381fd91fb08722c9a09076cc032706a5914bc8
AndrolinRoot <AndrolinRoot@googlemail.com>
2013-11-20 18:04:42 +0100

    Add Internal Illusion  version string into Build.java
    This can be accessed in any app using
    
    - android.os.Build.VERSION.illusion
    
    and corresponds to
    
    - ro.illusion.version in build.prop
    
    Change-Id: Ife7dc54f3da0d27b2f389078e206d17f1ffccc95

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -112,6 +112,12 @@ public class Build {
         public static final String CODENAME = getString("ro.build.version.codename");
 
         /**
+         * The current version string that defines the illusion version
+         * the device is on
+         */
+        public static final String Illusion = getString("ro.illusion.version");
+
+        /**
          * The SDK version to use when accessing resources.
          * Use the current SDK version code.  If we are a development build,
          * also allow the previous SDK version + 1.
